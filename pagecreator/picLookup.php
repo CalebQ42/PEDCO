ff42d9bf75a4c3147ccc7bf12c1d84778887a154
Belac Darkstorm <calebq23@gmail.com>
2016-05-16 13:53:28 -0500

    Added picture lookup :)

---
+++ pagecreator/picLookup.php
@@ -0,0 +1,11 @@
+<?php
+$loc = "../Pics";
+$pics = scandir($loc);
+foreach ($pics as $v){
+  if (strlen($v) >= 4){
+    if (substr($v,strlen($v)-4,strlen($v)-1)==".jpg" || substr($v,strlen($v)-4,strlen($v)-1)==".png" || substr($v,strlen($v)-5,strlen($v)-1)==".jpeg"){
+      echo '<option value="'.$loc.'/'.$v.'">'.$v.'</option>';
+    }
+  }
+}
+?>
